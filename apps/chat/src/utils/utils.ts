afd412ee3114617c14df419ffd4f2960ca72f982
xuchangheng <xuchangheng@goldenalpha.com.cn>
2023-06-27 10:50:04 +0800

    clipboard failure 去focus

--- apps/chat/src/utils/utils.ts
+++ apps/chat/src/utils/utils.ts
@@ -17,8 +17,8 @@ export function copyToClipboard(text: string) {
         textArea.value = text
         // 使text area不在viewport，同时设置不可见
         document.body.appendChild(textArea)
-        textArea.focus()
-        textArea.select()
+        // textArea.focus()
+        // textArea.select()
         return new Promise<void>((resolve, reject) => {
             // 执行复制命令并移除文本框
             document.execCommand('copy') ? resolve() : reject(new Error('出错了'))
