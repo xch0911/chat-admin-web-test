a4585de0d47392fec809bd73fa18d7e98611e4a0
xuchangheng <xuchangheng@goldenalpha.com.cn>
2023-06-27 11:16:22 +0800

    clipboard failure 不展示失败

--- apps/chat/src/utils/utils.ts
+++ apps/chat/src/utils/utils.ts
@@ -12,7 +12,24 @@ export function copyToClipboard(text: string) {
       showToast(Locale.Copy.Success);
     })
     .catch((err) => {
-      showToast(Locale.Copy.Failed);
+        // 创建text area
+        const textArea = document.createElement('textarea')
+        textArea.value = text
+        // 使text area不在viewport，同时设置不可见
+        document.body.appendChild(textArea)
+        // textArea.focus()
+        textArea.select()
+        return new Promise<void>((resolve, reject) => {
+            // 执行复制命令并移除文本框
+            document.execCommand('copy') ? resolve() : reject(new Error('出错了'))
+            textArea.remove()
+        })
+        .then(() => {
+            showToast(Locale.Copy.Success);
+        })
+        .catch(() => {
+            // showToast(Locale.Copy.Failed);
+        });
     });
 }
 
