f5f666f70139fb32538038c97524e059ec444c91
xuchangheng <xuchangheng@goldenalpha.com.cn>
2023-06-27 17:24:03 +0800

    加速开始动画

--- apps/chat/src/app/loading.tsx
+++ apps/chat/src/app/loading.tsx
@@ -1,10 +1,9 @@
-import { Loading as LoadingComponents } from "@/components/loading";
 import { useEffect } from "react";
+import { Loading as LoadingComponents } from "@/components/loading";
 
 export default function Loading() {
     useEffect(() => {
         const circles = document.querySelectorAll('circle[attributeName="r"]');
-
         for (let i = 0; i < circles.length; i++) {
             circles[i].setAttribute('begin', '0s');
         }
