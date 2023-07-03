2b0190c740faab522e2ed4e8d0d25e04cf4fff09
xuchangheng <xuchangheng@goldenalpha.com.cn>
2023-07-03 16:04:09 +0800

    自动注册账户尝试

--- apps/chat/src/app/enter/page.tsx
+++ apps/chat/src/app/enter/page.tsx
@@ -1,30 +1,70 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import {FormEvent, useState} from "react";
+import {useRouter, useSearchParams} from "next/navigation";
+import { showToast } from "@/components/ui-lib";
+import { useUserStore } from "@/store";
+import {RegisterResponse, ResponseStatus} from "@/app/api/typing.d";
+import {Loading} from "@/components/loading";
+
 
 import styles from "@/app/login/login.module.scss";
 import Locales from "@/locales";
 
-export default function Index() {
-  const router = useRouter();
 
-  return (
-    <div className={styles["content"]}>
-      <h2 className={styles["text"]}>AI BOT</h2>
-      <div className={styles["buttons"]}>
-        <button
-          className={styles["button"]}
-          onClick={() => router.push("/login")}
-        >
-          {Locales.Index.Login}
-        </button>
-        <button
-          className={styles["button"]}
-          onClick={() => router.push("/register")}
-        >
-          {Locales.Index.Register}
-        </button>
-      </div>
-    </div>
-  );
+export default function Index() {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const email = 'xx@aa.com';
+    const password = '123456';
+    const verificationCode = '112233';
+    const [invitationCode, setInvitationCode] = useState(
+        searchParams.get("code") ? ? ""
+    );
+    const [updateSessionToken, updateEmail] = useUserStore((state) => [
+        state.updateSessionToken,
+        state.updateEmail,
+    ]);
+
+    const handleRegister = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const res = (await (
+            await fetch("/api/user/register", {
+                cache: "no-store",
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({
+                    email: email.trim(),
+                    password,
+                    code: verificationCode,
+                    code_type: "email",
+                    invitation_code: invitationCode.toLowerCase() ? ? "",
+                }),
+            })
+        ).json()) as RegisterResponse;
+
+        switch (res.status) {
+            case ResponseStatus.Success: {
+                updateSessionToken(res.sessionToken);
+                updateEmail(email);
+                router.replace("/");
+                showToast(Locales.Index.Success(Locales.Index.Register), 3000);
+                break;
+            }
+            case ResponseStatus.alreadyExisted: {
+                showToast(Locales.Index.DuplicateRegistration);
+                break;
+            }
+            case ResponseStatus.invalidCode: {
+                showToast(Locales.Index.CodeError);
+                break;
+            }
+            default: {
+                showToast(Locales.UnknownError);
+                break;
+            }
+        }
+    };
+    return <Loading/>;
 }
