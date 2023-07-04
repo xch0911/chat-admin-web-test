716e8be71d9672a7d55f31964131edbf016c2af7
xuchangheng <xuchangheng@goldenalpha.com.cn>
2023-07-04 19:20:03 +0800

    测试 spark

--- packages/bots/src/spark.ts
+++ packages/bots/src/spark.ts
@@ -9,19 +9,20 @@ export class SparkBot extends AbstractBot {
     }
 
     protected async * doAnswer({conversation, signal}: AnswerParams,): AsyncIterable<string> {
-        const userMessage = conversation.at(-1);
-        if (!userMessage) {
-            throw new Error("User message not found");
-        }
-        console.log(REQUEST_URL + "?q=" + userMessage.content + "&u=" + this.email);
-        const response = await fetch(REQUEST_URL + "?q=" + userMessage.content + "&u=" + this.email, {
-            method: "GET",
-        });
-        console.log(response);
-
-        if (!response.ok) {
-            throw new Error(`${response.statusText}: ${await response.text()}`);
-        }
-        return await response.text()
+        // const userMessage = conversation.at(-1);
+        // if (!userMessage) {
+        //     throw new Error("User message not found");
+        // }
+        // console.log(REQUEST_URL + "?q=" + userMessage.content + "&u=" + this.email);
+        // const response = await fetch(REQUEST_URL + "?q=" + userMessage.content + "&u=" + this.email, {
+        //     method: "GET",
+        // });
+        // console.log(response);
+        //
+        // if (!response.ok) {
+        //     throw new Error(`${response.statusText}: ${await response.text()}`);
+        // }
+        // return  response.text()
+        return "1112233"
     }
 }
