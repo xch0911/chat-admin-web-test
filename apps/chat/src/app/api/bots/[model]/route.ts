564dffbd265840a2ad5d7937a2538d2f34a4c3d5
xuchangheng <xuchangheng@goldenalpha.com.cn>
2023-07-04 18:03:16 +0800

    测试spark

--- apps/chat/src/app/api/bots/[model]/route.ts
+++ apps/chat/src/app/api/bots/[model]/route.ts
@@ -13,6 +13,7 @@ export async function POST(req: NextRequest,
     const email = req.headers.get("email")!;
 
     console.debug("[Route] [Bots]", email);
+    console.debug( params.model);
 
     let payload;
 
