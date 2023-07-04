a03d0480dbb15c403b87e944b2bfff49fa8418df
xuchangheng <xuchangheng@goldenalpha.com.cn>
2023-07-04 19:24:34 +0800

    测试 spark

--- packages/bots/src/spark.ts
+++ packages/bots/src/spark.ts
@@ -1,5 +1,6 @@
 import {AbstractBot} from "./abstract-bot";
 import {AnswerParams} from "./types";
+import { streamToLineIterator } from "./utils";
 
 const REQUEST_URL = "http://grab3.arfgc.com:8028/spark";
 
@@ -9,20 +10,21 @@ export class SparkBot extends AbstractBot {
     }
 
     protected async * doAnswer({conversation, signal}: AnswerParams,): AsyncIterable<string> {
-        // const userMessage = conversation.at(-1);
-        // if (!userMessage) {
-        //     throw new Error("User message not found");
-        // }
-        // console.log(REQUEST_URL + "?q=" + userMessage.content + "&u=" + this.email);
-        // const response = await fetch(REQUEST_URL + "?q=" + userMessage.content + "&u=" + this.email, {
-        //     method: "GET",
-        // });
-        // console.log(response);
-        //
-        // if (!response.ok) {
-        //     throw new Error(`${response.statusText}: ${await response.text()}`);
-        // }
-        // return  response.text()
-        return "1112233"
+        const userMessage = conversation.at(-1);
+        if (!userMessage) {
+            throw new Error("User message not found");
+        }
+        console.log(REQUEST_URL + "?q=" + userMessage.content + "&u=" + this.email);
+        const response = await fetch(REQUEST_URL + "?q=" + userMessage.content + "&u=" + this.email, {
+            method: "GET",
+        });
+        console.log(response);
+
+        if (!response.ok) {
+            throw new Error(`${response.statusText}: ${await response.text()}`);
+        }
+        for await (const line of streamToLineIterator(response.body!)) {
+            yield line;
+        }
     }
 }
