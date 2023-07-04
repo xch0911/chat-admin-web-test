eaaee80491cff26eaef8474a62b9c2ebbab66976
xuchangheng <xuchangheng@goldenalpha.com.cn>
2023-07-04 20:15:52 +0800

    测试 spark

--- apps/chat/src/app/api/bots/[model]/types.ts
+++ apps/chat/src/app/api/bots/[model]/types.ts
@@ -1,4 +1,9 @@
+export type ChatRole = "assistant" | "user" | "system" | "context";
 
+export type ChatRecord = {
+    role: ChatRole;
+    content: string;
+};
 export interface AnswerParams {
   conversation: ChatRecord[];
   maxTokens?: number;
